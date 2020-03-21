c3f12b4228f5d879693b75bc2f4a86de627a6fce
dilek-technostudy <dileknuranyildirim@gmail.com>
2020-03-21 10:36:44 -0700

    Dilek's part

---
+++ src/test/java/stepDefinitions/personalInfoSteps.java
@@ -0,0 +1,56 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import pageObjectModel.AbstractClass;
+import pageObjectModel.personalInfoPage;
+
+public class personalInfoSteps extends AbstractClass {
+
+    personalInfoPage ip = new personalInfoPage();
+
+
+    @Given("^Click on My Personal Information$")
+    public void click_on_My_Personal_Information() throws Throwable {
+        ip.clickOnMyPersonalInfoTab();
+    }
+
+    @Given("^Click on social title$")
+    public void click_on_social_title() throws Throwable {
+        ip.clickOnSocialTitle();
+
+    }
+
+    @Given("^type to\"([^\"]*)\"and \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void type_to_and_and(String arg1, String arg2, String arg3) throws Throwable {
+
+        ip.typeFirstName(arg1);
+
+        ip.typeLastName(arg2);
+
+        ip.typeNewEmail(arg3);
+        waitMethod();
+
+        scrollDown();
+
+    }
+
+    @Given("^type \"([^\"]*)\"and \"([^\"]*)\"$")
+    public void type_and(String password, String newPassWord) throws Throwable {
+
+        ip.typeOldPassWord(password);
+        waitMethod();
+
+        ip.inputnewPassWord(newPassWord);
+
+
+    }
+
+    @Given("^Click on Save button$")
+    public void click_on_Save_button() throws Throwable {
+        waitMethod();
+
+        ip.clickOnSaveButton();
+    }
+}
---
+++ src/test/resources/personalInfo.feature
@@ -0,0 +1,14 @@
+Feature:Editing personal information
+  Scenario: Editing personal information
+
+    Given Go to the website
+    And Click on Sign In Tab
+    And type to "email" and "password"
+    And Click on Sign In Button
+    And Click on My Personal Information
+    And Click on social title
+    And type to"First name"and "Last name" and "E-mail address"
+    And type "Current Password"and "New Password"
+    And Click on Save button
+
+
